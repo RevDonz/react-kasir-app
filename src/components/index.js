46a04db552a5f210a6360d1a30f7a566ca0724ab
RevDonz <aprilio842@gmail.com>
2021-04-20 20:07:35 +0700

    menampilkan daftar makanan

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Hasil from './Hasil'
+import Menus from './Menus'
+import ListCategory from './ListCategory'
+import NavbarComponent from './NavbarComponent'
+
+export { Hasil, ListCategory, NavbarComponent, Menus }
